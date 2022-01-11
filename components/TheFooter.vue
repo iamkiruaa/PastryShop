ffbe97bbe9dae013ad8caa62f6ae2d14c899549c
iamkiruaa <kouahofrancis@gmail.com>
2022-01-11 22:48:46 +0100

    add TheFooter.vue

---
+++ components/TheFooter.vue
@@ -0,0 +1,49 @@
+<template>
+  <footer class="footer-main container-fluid no-padding">
+    <div class="footer-content container">
+      <div class="col-md-4 col-sm-6 col-xs-6">
+        <div class="about-footer">
+          <h5>Commander en ligne</h5>
+          <p>La journée pour un tour de trois heures un tour de trois heures puis un jour il tirait sur de la nourriture et à travers le le sol de l'huile.</p>
+          <a href="#" title="Order Now" class="about-links order">Commandez maintenant</a>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-6 col-xs-6">
+        <div class="about-footer">
+          <h5>Contactez nous aujourd'hui</h5>
+          <p>Appelez-nous 0666340351</p>
+          <p>Envoyez un courriel à contact@olivia.com</p>
+          <p>Visitez-nous 119 rue Saint Sebastien 78300, Poissy</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bottom Footer -->
+  <div class="bottom-footer container-fluid no-padding">
+    <div class="container">
+      <div class="footer-copyright">
+        <p>&copy; Copyrights 2021 Olivia OCCANSEY. Tous droits réservés</p>
+      </div>
+      <div class="footer-breadcrumb pull-right">
+        <ol class="breadcrumb">
+          <li><a href="#" title="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+          <li><a href="#" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+          <li><a href="#" title="Google-plus"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+          <li><a href="#" title="Dribbble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+        </ol>
+      </div>
+    </div>
+  </div><!-- Bottom Footer /- -->
+</template>
+
+<script>
+export default {
+  name: "TheFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
