731cc02b5fa6c305fc06ff0641206adc0c2856b3
iamkiruaa <kouahofrancis@gmail.com>
2022-01-11 23:53:04 +0100

    add TeamSection.vue

---
+++ components/TeamSection.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- Team Section -->
+  <div class="team-section container-fluid no-padding">
+    <div class="section-padding"></div>
+    <div class="container">
+      <div class="row">
+        <!-- Main Carousel -->
+        <div id="main-carousel1" class="carousel slide carousel-fade" data-ride="carousel">
+          <div role="listbox" class="carousel-inner">
+            <div class="item active">
+              <div class="team-content">
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                  <img src="images/team.png" alt="team" width="468" height="595"/>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                  <div class="section-header left-header">
+                    <h3>Meet Our Team</h3>
+                    <h5>CHEFS</h5>
+                    <img src="images/section-seprator-1.png" alt="section-seprator" width="70" height="3">
+                    <p>The day for a three hour tour a three hour tour then one day he was shooting at some food and up
+                      through the ground came a oil that is its a beautiful day Got a dream and we just know now we are
+                      gonna make our dream come true so get a witch is shawl on a broomstick you can crawl on were gonna
+                      pay.</p>
+                  </div><!-- Section Header /-  -->
+                  <div class="team-contact-info">
+                    <h5>Stefhen Richards</h5>
+                    <ul>
+                      <li><a href="#" title="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                      <li><a href="#" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                      <li><a href="#" title="Google-plus"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                      <li><a href="#" title="linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                      <li><a href="#" title="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    </ul>
+                    <p><a href="mailto:info@domain.com" title="info@domain.com"><i class="fa fa-envelope"
+                                                                                   aria-hidden="true"></i>info@domain.com</a>
+                    </p>
+                    <p><a href="tel:1-800-23-456-7890" title="1-800-23-456-7890"><i class="fa fa-phone"
+                                                                                    aria-hidden="true"></i>Phone:
+                      1-800-23-456-7890</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Container /- -->
+  </div>
+  <!-- Team Section /- -->
+
+</template>
+
+<script>
+export default {
+  name: "TeamSection"
+}
+</script>
+
+<style scoped>
+
+</style>
