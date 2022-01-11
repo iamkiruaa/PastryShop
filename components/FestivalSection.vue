6de3aefb6213a1630f82b21ca8043a82033bcb6e
iamkiruaa <kouahofrancis@gmail.com>
2022-01-11 23:34:15 +0100

    add FestivalSection.vue

---
+++ components/FestivalSection.vue
@@ -0,0 +1,88 @@
+<template>
+  <!-- Festival Section -->
+  <div class="festival-section container-fluid no-padding">
+    <div class="section-padding"></div>
+    <div class="container">
+      <div class="festival-details-tab">
+        <!-- Tab panes -->
+        <div class="tab-content">
+          <div role="tabpanel" class="tab-pane active" id="partycakes">
+            <div class="festival-content">
+              <h5>Festival du gâteau</h5>
+              <img src="images/section-seprator-1.png" alt="section-seprator-1" width="70" height="3"/>
+              <p>Le jour pour un tour de trois heures un tour de trois heures puis un jour il tirait sur de la
+                nourriture et à travers le sol est apparu un pétrole. à travers le sol est apparu un pétrole qui est son
+                un beau jour obtenu un rêve vrai.</p>
+              <a href="#" title="Lire la suite" class="read-more">Lire la suite</a>
+            </div>
+          </div>
+          <div role="tabpanel" class="tab-pane" id="softcakes">
+            <div class="festival-content">
+              <h5>softcakes</h5>
+              <img src="images/section-seprator-1.png" alt="section-seprator-1" width="70" height="3"/>
+              <p>Le jour pour un tour de trois heures un tour de trois heures puis un jour il tirait sur de la
+                nourriture et à travers le sol est apparu un pétrole. à travers le sol est apparu un pétrole qui est son
+                un beau jour obtenu un rêve vrai.</p>
+              <a href="#" title="Lire la suite" class="read-more">Lire la suite</a>
+            </div>
+          </div>
+          <div role="tabpanel" class="tab-pane" id="cupcakes">
+            <div class="festival-content">
+              <h5>cupcakes</h5>
+              <img src="images/section-seprator-1.png" alt="section-seprator-1" width="70" height="3"/>
+              <p>Le jour pour un tour de trois heures un tour de trois heures puis un jour il tirait sur de la
+                nourriture et à travers le sol est apparu un pétrole. à travers le sol est apparu un pétrole qui est son
+                un beau jour obtenu un rêve vrai.</p>
+              <a href="#" title="Lire la suite" class="read-more">Lire la suite</a>
+            </div>
+          </div>
+          <div role="tabpanel" class="tab-pane" id="berrycakes">
+            <div class="festival-content">
+              <h5>berrycakes</h5>
+              <img src="images/section-seprator-1.png" alt="section-seprator-1" width="70" height="3"/>
+              <p>Le jour pour un tour de trois heures un tour de trois heures puis un jour il tirait sur de la
+                nourriture et à travers le sol est apparu un pétrole. à travers le sol est apparu un pétrole qui est son
+                un beau jour obtenu un rêve vrai.</p>
+              <a href="#" title="Lire la suite" class="read-more">Lire la suite</a>
+            </div>
+          </div>
+          <div role="tabpanel" class="tab-pane" id="nutcake">
+            <div class="festival-content">
+              <h5>nutcake</h5>
+              <img src="images/section-seprator-1.png" alt="section-seprator-1" width="70" height="3"/>
+              <p>Le jour pour un tour de trois heures un tour de trois heures puis un jour il tirait sur de la
+                nourriture et à travers le sol est apparu un pétrole. à travers le sol est apparu un pétrole qui est son
+                un beau jour obtenu un rêve vrai.</p>
+              <a href="#" title="Lire la suite" class="read-more">Lire la suite</a>
+            </div>
+          </div>
+        </div>
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+          <li role="presentation" class="active"><a href="#partycakes" aria-controls="partycakes " role="tab"
+                                                    data-toggle="tab"><i><img src="images/festival-ic-1.png"
+                                                                              alt="festival-ic-1" width="37"
+                                                                              height="42"/></i>partycakes</a></li>
+          <li role="presentation"><a href="#softcakes" aria-controls="softcakes" role="tab" data-toggle="tab"><i><img
+              src="images/festival-ic-2.png" alt="festival-ic-2" width="37" height="42"/></i>softcakes</a></li>
+          <li role="presentation"><a href="#cupcakes" aria-controls="cupcakes" role="tab" data-toggle="tab"><i><img
+              src="images/festival-ic-3.png" alt="festival-ic-3" width="37" height="42"/></i>cupcakes</a></li>
+          <li role="presentation"><a href="#berrycakes" aria-controls="berrycakes" role="tab" data-toggle="tab"><i><img
+              src="images/festival-ic-4.png" alt="festival-ic-4" width="37" height="42"/></i>berrycakes</a></li>
+          <li role="presentation"><a href="#nutcake" aria-controls="nutcake" role="tab" data-toggle="tab"><i><img
+              src="images/festival-ic-5.png" alt="festival-ic-5" width="37" height="42"/></i>nutcake</a></li>
+        </ul>
+      </div>
+    </div><!-- Container /- -->
+  </div><!-- Festival Section /- -->
+</template>
+
+<script>
+export default {
+  name: "FestivalSection"
+}
+</script>
+
+<style scoped>
+
+</style>
