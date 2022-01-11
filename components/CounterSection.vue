d31e3838983e198284ff0460f827b26aa07b6b4b
iamkiruaa <kouahofrancis@gmail.com>
2022-01-12 00:05:29 +0100

    add CounterSection.vue

---
+++ components/CounterSection.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- Counter Section -->
+  <div class="counter-section container-fluid no-padding">
+    <div class="container">
+      <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="counter-box">
+          <h3><span class="count" id="statistics_count-1" data-statistics_percent="1500"> &nbsp;</span></h3>
+          <i class="fa fa-smile-o" aria-hidden="true"></i>
+          <p>CLIENTS SATISFAITS</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="counter-box">
+          <h3><span class="count" id="statistics_count-2" data-statistics_percent="550">&nbsp;</span></h3>
+          <i class="fa fa-coffee" aria-hidden="true"></i>
+          <p>TASSES DE CAFÉ</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="counter-box">
+          <h3><span class="count" id="statistics_count-3" data-statistics_percent="750">&nbsp;</span></h3>
+          <i class="fa fa-hourglass-o" aria-hidden="true"></i>
+          <p>HEURES TRAVAILLÉES</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="counter-box">
+          <h3><span class="count" id="statistics_count-4" data-statistics_percent="110">&nbsp;</span></h3>
+          <i class="fa fa-home" aria-hidden="true"></i>
+          <p>BRANCHES</p>
+        </div>
+      </div>
+    </div>
+  </div><!-- Counter Section /- -->
+</template>
+
+<script>
+export default {
+  name: "CounterSection"
+}
+</script>
+
+<style scoped>
+
+</style>
