7578f713607f6ac3b2acc6481d36989e44358f43
iamkiruaa <kouahofrancis@gmail.com>
2022-01-12 22:38:09 +0100

    *wip backup

--- components/MainCarousel.vue
+++ components/MainCarousel.vue
@@ -5,32 +5,29 @@
     <div id="main-carousel" class="carousel slide carousel-fade" data-ride="carousel">
       <div role="listbox" class="carousel-inner">
         <div class="item active">
-          <img width="1920" height="1030" alt="slider" src="images/slider-1.jpg">
+          <img width="1920" height="1030" alt="slider" src="assets/images/slider-1.jpg">
           <div class="carousel-caption">
-            <img data-animation="animated pulse" src="images/slider-ic.png" alt="slider-ic" width="121" height="124">
-            <p data-animation="animated bounceInLeft">Cake for Later Cake </p>
-            <p data-animation="animated bounceInRight">as a way of Life</p>
-            <a title="Read More" href="#" data-animation="animated bounceInUp">Read More</a>
+            <img data-animation="animated pulse" src="assets/images/slider-ic.png" alt="slider-ic" width="121" height="124">
+            <p data-animation="animated bounceInLeft">Gâteau pour plus tard Gâteau </p>
+            <p data-animation="animated bounceInRight">comme mode de vie</p>
           </div>
         </div>
 
         <div class="item">
-          <img width="1920" height="1030" alt="slider" src="images/slider-2.jpg">
+          <img width="1920" height="1030" alt="slider" src="assets/images/slider-2.jpg">
           <div class="carousel-caption">
-            <img data-animation="animated pulse" src="images/slider-ic.png" alt="slider-ic" width="121" height="124">
-            <p data-animation="animated bounceInLeft">Cake for Later Cake </p>
-            <p data-animation="animated bounceInRight">as a way of Life</p>
-            <a title="Read More" href="#" data-animation="animated bounceInUp">Read More</a>
+            <img data-animation="animated pulse" src="assets/images/slider-ic.png" alt="slider-ic" width="121" height="124">
+            <p data-animation="animated bounceInLeft">Gâteau pour plus tard Gâteau </p>
+            <p data-animation="animated bounceInRight">comme mode de vie</p>
           </div>
         </div>
 
         <div class="item">
-          <img width="1920" height="1030" alt="slider" src="images/slider-3.jpg">
+          <img width="1920" height="1030" alt="slider" src="assets/images/slider-3.jpg">
           <div class="carousel-caption">
-            <img data-animation="animated pulse" src="images/slider-ic.png" alt="slider-ic" width="121" height="124">
-            <p data-animation="animated bounceInLeft">Cake for Later Cake </p>
-            <p data-animation="animated bounceInRight">as a way of Life</p>
-            <a title="Read More" href="#" data-animation="animated bounceInUp">Read More</a>
+            <img data-animation="animated pulse" src="assets/images/slider-ic.png" alt="slider-ic" width="121" height="124">
+            <p data-animation="animated bounceInLeft">Gâteau pour plus tard Gâteau </p>
+            <p data-animation="animated bounceInRight">comme mode de vie</p>
           </div>
         </div>
         <a class="left carousel-control" href="#main-carousel" role="button" data-slide="prev">
